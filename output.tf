d90aab01694396364269b48d2e3d324c0a3c1735
bernard <bernard@traveloka.com>
2018-10-16 19:44:15 +0700

    Add output for instance role.

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "instance_id for bastion hosts"
   value       = "${aws_instance.this.id}"
 }
+
+output "instance_role_name" {
+  description = "role name for the instances."
+  value       = "${module.instance_profile.role_name}"
+}
